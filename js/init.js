7476e8f2fd1681fb46d1c7ce2ab0f61bd130ed03
Recky <Recky>
2016-07-14 16:00:26 +0300

    GMap update

--- js/init.js
+++ js/init.js
@@ -18,6 +18,7 @@ $(document).ready(function() {
          fitToSection: true,
          fixedElements: '.wrapper-popup'
 
+
     });
 
     bindEvents();
@@ -36,9 +37,9 @@ $(document).ready(function() {
          });
 
         carouselCount ++;
-//        console.log(carouselCount);
         });
 
+        initGMap();
     });
 
 function bindEvents(){
@@ -87,7 +88,52 @@ function resetScrollPosition(){
     "-ms-transform":"translate(0px,0px)",
     "transform":"translate(0px,0px)"
   });
-}
+};
+
+var map;
+
+
+
+function initGMap() {
+    // Create an array of styles.
+  var styles = [{"stylers":[{"saturation":-100},{"gamma":0.8},{"lightness":4},{"visibility":"on"}]},{"featureType":"landscape.natural","stylers":[{"visibility":"on"},{"color":"#5dff00"},{"gamma":4.97},{"lightness":-5},{"saturation":100}]}];
+
+  // Create a new StyledMapType object, passing it the array of styles,
+  // as well as the name to be displayed on the map type control.
+  var styledMap = new google.maps.StyledMapType(styles,
+    {name: "Styled Map"});
+
+   var myPlacemark = {lat: 55.7885499, lng: 37.72956881};
+
+    var image = 'img/marker.png';
+
+    var marker = new google.maps.Marker({
+    position: myPlacemark,
+    title:"TotalMarketing:Digital",
+    icon: image
+    });
+
+
+
+  // Create a map object, and include the MapTypeId to add
+  // to the map type control.
+  var mapOptions = {
+    zoom: 17,
+    center: new google.maps.LatLng(55.78869686104115, 37.72956881),
+    mapTypeControlOptions: {
+      mapTypeIds: [google.maps.MapTypeId.ROADMAP, 'map_style']
+    }
+  };
+  var map = new google.maps.Map(document.getElementById('gmap'),
+    mapOptions);
+
+  //Associate the styled map with the MapTypeId and set it to display.
+  map.mapTypes.set('map_style', styledMap);
+  map.setMapTypeId('map_style');
+
+  // To add the marker to the map, call setMap();
+  marker.setMap(map);
+};
 
 
 
